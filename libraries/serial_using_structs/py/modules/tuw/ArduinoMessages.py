2b605ef4ec8e24d96c1aa997faa79f6850f982f3
Markus Bader @ munin <markus.bader@tuwien.ac.at>
2016-04-13 09:33:57 +0200

    python struct as static member of serial structs

---
+++ libraries/serial_using_structs/py/modules/tuw/ArduinoMessages.py
@@ -0,0 +1,52 @@
+## @package ArduinoMessages
+#  defines messages for serial communication
+
+
+import struct
+import copy
+    
+## class to receive and send via serial link
+class Pose:
+    
+    ## unique type id to identify received objects
+    TYPE = 100
+    struct = struct.Struct("fff")
+    
+    
+    ## constructor
+    def __init__(self, x = 0., y = 0., theta = 0.):
+        self.x = x
+        self.y = y
+        self.theta = theta
+
+    ## de-serialized a received string into the class variables
+    def unpack(self, msg):
+        self.x, self.y, self.z = self.struct.unpack(msg)
+        
+    ## serializes the class variables into a buffer 
+    # @return serialized buffer
+    def pack(self):
+        msg = self.struct.pack(self.x, self.y, self.theta)
+        return msg
+           
+## class to receive and send via serial link
+class Text:
+    
+    ## unique type id to identify received objects
+    TYPE = 200
+    struct = struct.Struct("64s")
+    
+    
+    ## constructor
+    def __init__(self, text = ''):
+        self.text = copy.copy(text)
+
+    ## de-serialized a received string into the class variables
+    def unpack(self, msg):
+        self.text = copy.copy(msg)
+        
+    ## serializes the class variables into a buffer 
+    # @return serialized buffer
+    def pack(self):
+        return copy.copy(self.text) 
+        
